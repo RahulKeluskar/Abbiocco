187add06d1de01da5f5281850a837a417eb09d68
Rahul <rahul.keluskar@sap.com>
2021-09-18 01:13:22 +0530

    changes for demo 1

--- app/email.py
+++
@@ -1,6 +0,0 @@
-class Email:
-    """
-    Scope for development: Sending notifications to the user via email
-    """
-    def __init__(self):
-        pass
--- app/erorrs.py
+++
@@ -1,6 +0,0 @@
-class Error:
-    """
-    Scope of this class is a helper class storing all the errors that might occur in the application
-    """
-    def __init__(self):
-        pass
